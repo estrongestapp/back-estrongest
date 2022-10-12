8e1e488d6c06272db6e3b656585c3d483e07e437
lfelipelizeu <l_felipelizeu@hotmail.com>
2022-10-12 19:39:29 -0300

    remove dist from git

--- dist/src/errors/ConflictError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ConflictError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = 'ConflictError';
-    }
-}
-exports.default = ConflictError;
--- dist/src/validations/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserSchema = void 0;
-const UserValidationSchema_1 = __importDefault(require("./UserValidationSchema"));
-exports.UserSchema = UserValidationSchema_1.default;
