f6e5907a4268811bf82bf4baab4ecaef0cf83259
lfelipelizeu <l_felipelizeu@hotmail.com>
2023-01-04 20:07:10 -0300

    fix headers

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,13 @@ import connectDatabase from './database';
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 
 app.use('/user', userRouter);
 app.use('/infos', infosRouter);
